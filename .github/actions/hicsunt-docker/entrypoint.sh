ae7d835c970369951d31614f69089dcc968624af
Kenneth Pouncey <kjpou@pt.lu>
2022-01-13 07:24:52 +0100

    custom hicsunt docker action 3

--- .github/actions/hicsunt-docker/entrypoint.sh
+++ .github/actions/hicsunt-docker/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -l
 
+echo "::debug ::Debug Message"
+echo "::warning ::Warning Message"
+echo "::error ::Error Message"
+
+# echo "::add-mask::$1"
 echo "Hic Sunt $1"
 time=$(date)
 echo "::set-output name=time::$time"
+
+echo "::group::Some expandable log"
+echo "some stuff"
+echo "::endgroup::"
